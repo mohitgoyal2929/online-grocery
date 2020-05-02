7f2652a7790386cf53c028d9b797c44b95223bf5
Mohit Goyal <mohitgoyal2929@gmail.com>
2020-05-02 15:44:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h3>from app</h3>
+    </React.Fragment>
+  );
+}
+
+export default App;
