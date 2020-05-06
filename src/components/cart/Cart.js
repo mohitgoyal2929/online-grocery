8c8e5ef7f34c93e7f0622ea1a6f2d3f362a6ffe6
Mohit Goyal <mohitgoyal2929@gmail.com>
2020-05-06 04:35:26 -0400

    Cart Styles

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Title from "../Title";
 import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../../Context";
+import CartList from "./CartList";
 
 export default function Cart() {
   return (
     <section>
-      <Title name="your" title="cart" />
-      <CartColumns />
+      <ProductConsumer>
+        {(value) => {
+          const { cart } = value;
+          if (cart.length > 0)
+            return (
+              <React.Fragment>
+                <Title name="your" Title="cart" />
+                <CartColumns />
+                <CartList value={value} />
+              </React.Fragment>
+            );
+          else return <EmptyCart />;
+        }}
+      </ProductConsumer>
     </section>
   );
 }
